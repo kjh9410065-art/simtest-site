5f63f452d0f0a7708655b9b9360eba5bf84917d6
kjh9410065-art <kjh9410065@gmail.com>
2026-09-10 00:16:05 +0900

    이미지 연결 Worker 활성화

--- worker.js
+++ worker.js
@@ -1,7 +1,6 @@
 import core from './worker-core.js';
 import { addImageBridge } from './image-bridge.js';
 
-// 기존 Worker 기능을 유지하면서 HTML에 생성된 사이트 이미지를 연결합니다.
 export default {
   async fetch(request, env, ctx) {
     const response = await core.fetch(request, env, ctx);
